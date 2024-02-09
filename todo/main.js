a0bb09ca3f5e770d42d5c5e3003a07a3f45aeb6a
blinmakersha <valentinakulg@gmail.com>
2024-02-09 15:52:42 +0300

    feat: add dom tree for todo-app

---
+++ todo/main.js
@@ -0,0 +1,48 @@
+(function () {
+  function createAppTitle(title) {
+    let appTitle = document.createElement("h2");
+    appTitle.innerHTML = title;
+    return appTitle;
+  }
+
+  function createTodoItemForm() {
+    let form = document.createElement("form");
+    let input = document.createElement("input");
+    let buttonWrapper = document.createElement("div");
+    let button = document.createElement("button");
+
+    form.classList.add("input-group", "mb-3");
+    input.classList.add("form-control");
+    input.placeholder = "Enter new todo";
+    buttonWrapper.classList.add("input-group-append");
+    button.classList.add("btn", "btn-primary");
+    button.textContent = "Add todo";
+
+    buttonWrapper.append(button);
+    form.append(input);
+    form.append(buttonWrapper);
+
+    return {
+      form,
+      input,
+      button,
+    };
+  }
+
+  function createTodoList() {
+    let list = document.createElement("ul");
+    list.classList.add("list-group");
+    return list;
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    let container = document.getElementById("todo-app");
+    let todoAppTitle = createAppTitle("List of todos");
+    let todoItemForm = createTodoItemForm();
+    let todoList = createTodoList();
+
+    container.append(todoAppTitle);
+    container.append(todoItemForm.form);
+    container.append(todoList);
+  });
+})();
